030d61949a8399aa1321cb3c769ae8283a7dcbfb
Guilherme Lisboa <guilhermelisboa0106@gmail.com>
2025-11-19 12:14:17 -0300

    Tela de cadastro e login parcialmente prontas

--- CadastroCliente.php
+++ CadastroCliente.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="cadastro.css">
+    <title>Cadastro</title>
 </head>
 <body>
 
@@ -13,23 +14,32 @@
         if(isset($_SESSION['msg'])){
             echo $_SESSION['msg'];
             unset($_SESSION['msg']);
-            
-            
-
         }
+        include('header.php');
     ?>
+    
     <form action="CadastroCliente.act.php" method="post">
-        <p>Nome: <input type="text" name="nome" id=""></p>
-        <p>Senha: <input type="text" name="senha" id=""></p>
-        <!-- <p>Cpf: <input type="number" name="cpf" id=""></p> -->
-        <!-- <p>rg: <input type="number" name="rg" id=""></p> -->
-        <!-- <p>Cidade <input type="text" name="cidade" id=""></p> -->
-        <!-- <p>Bairro <input type="text" name="bairro" id=""></p> -->
-        <!-- <p>Estado <input type="text" name="estado" id=""></p> -->
-        <!-- <p>Cep <input type="number" name="cep" id=""></p> -->
-        <!-- <p>Email: <input type="text" name="email" id=""></p> -->
-        <!-- <p>Telefone: <input type="text" name="telefone" id=""></p> -->
-        <!-- <p>Nascimento: <input type="date" name="nascto" id=""></p> -->
+        <div class="sessoes">
+            <a href="login.php"><h2>Entrar</h2></a>
+            <h2>|</h2>
+            <a href="CadastroCliente.php"><h2 class='cadastrar'>Cadastrar-se</h2></a>
+        </div>
+        <div class="user-box">
+            <Label>Seu email:</Label>
+            <input type="email" name="email" id="">
+            <Label>Sua senha:</Label> 
+            <input type="password" name="senha" id="">
+            <a href="#"><p>Esqueci minha senha</p></a>
+            <!-- <p>Cpf: <input type="number" name="cpf" id=""></p> -->
+            <!-- <p>rg: <input type="number" name="rg" id=""></p> -->
+            <!-- <p>Cidade <input type="text" name="cidade" id=""></p> -->
+            <!-- <p>Bairro <input type="text" name="bairro" id=""></p> -->
+            <!-- <p>Estado <input type="text" name="estado" id=""></p> -->
+            <!-- <p>Cep <input type="number" name="cep" id=""></p> -->
+            <!-- <p>Email: <input type="text" name="email" id=""></p> -->
+            <!-- <p>Telefone: <input type="text" name="telefone" id=""></p> -->
+            <!-- <p>Nascimento: <input type="date" name="nascto" id=""></p> -->
+        </div>
         <p class="botao"><input type="submit" value="Enviar" id="formCliente" onsubmit=""></p>
         <!-- <input type="submit" value="Gerar Relatorio"> -->
     </form>
--- login.php
+++ login.php
@@ -1,11 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cadastro.css">
+    <title>Login</title>
+</head>
+<body>
 
-
-if(isset($_SESSION)){
-    // if $_SERVE['idUsuario']
-}
-
-
-
-
-?>
+    <?php
+    
+        if(isset($_SESSION)){
+            // if $_SERVE['idUsuario']
+        }
+        header("Location: dashboard.php");
+        include('header.php');
+    ?>
+    <form action="CadastroCliente.act.php" method="post">
+        <div class="sessoes">
+            <a href="login.php"><h2>Login</h2></a>
+            <h2>|</h2>
+            <a href="CadastroCliente.php"><h2>Cadastro</h2></a>
+        </div>
+        <div class="user-box">
+            <Label>Email:</Label>
+            <input type="email" name="email" id="">
+            <Label>Senha</Label> 
+            <input type="password" name="senha" id="">
+        </div>
+        <p class="botao"><input type="submit" value="Enviar" id="formCliente" onsubmit=""></p>
+        <!-- <input type="submit" value="Gerar Relatorio"> -->
+    </form>
+</body>
+</html>
